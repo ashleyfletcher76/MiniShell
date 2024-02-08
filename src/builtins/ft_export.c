33890ac4360bf214503a3de21091f1f33079c2fe
Muhammet Kose <muhakose@3-F-1.42heilbronn.de>
2024-02-08 15:50:02 +0100

    ft_export

--- src/builtins/ft_export.c
+++ src/builtins/ft_export.c
@@ -6,7 +6,7 @@
 /*   By: muhakose <muhakose@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/05 10:57:10 by muhakose          #+#    #+#             */
-/*   Updated: 2024/02/05 10:59:58 by muhakose         ###   ########.fr       */
+/*   Updated: 2024/02/07 17:49:38 by muhakose         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,49 @@
 
 void	ft_export(char **commands, char **env)
 {
-	int i = 0;
+	int i;
+	int	equl_cnt;
 
-	while (commands[i] != NULL)
-		printf("%s\n", commands[i++]);
+	i = 0;
+	if (commands[1] == NULL)
+	{
+		while (env[i])
+			ft_printf("%s\n", env[i++]);
+		return ;
+	}
+	equl_cnt = ft_count_equal(commands[1]);
+	printf("%d\n", equl_cnt);
+	if (equl_cnt == 0)
+	{
+		env[i] = env[i - 1];
+		env[i - 1] = ft_strjoin(commands[1], "=");
+		env[i + 1] = NULL;
+	}
+	while(env[i] != NULL)
+	{
+		if (ft_strncmp(env[i], commands[1], equl_cnt) == 0)
+		{
+			ft_strcpy(env[i], commands[1]);
+			return ;
+		}
+		i++;
+	}
+	env[i] = env[i - 1];
+	env[i - 1] = commands[1];
+	env[i + 1] = NULL; 
+}
 
-	(void)env;
 
+int	ft_count_equal(char *s)
+{
+	int	i;
 
-
-
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == '=')
+			return (i + 1);
+		i++;
+	}
+	return (0);
 }
