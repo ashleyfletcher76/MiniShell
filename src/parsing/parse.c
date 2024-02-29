41cb44a45bbc82bc8e0ec0012251a389a6ac0d53
Muhammet Kose <muhakose@1-C-18.42heilbronn.de>
2024-02-29 16:47:36 +0100

    print

--- src/parsing/parse.c
+++ src/parsing/parse.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parse.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: asfletch <asfletch@student.42heilbronn.    +#+  +:+       +#+        */
+/*   By: muhakose <muhakose@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/29 15:57:35 by asfletch          #+#    #+#             */
-/*   Updated: 2024/02/29 15:57:37 by asfletch         ###   ########.fr       */
+/*   Updated: 2024/02/29 16:47:29 by muhakose         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -101,4 +101,47 @@ void	parse_init(t_mini *mini)
 	command = lstnew();
 	parse_distributor(mini, command, 0, 0);
 	exec_init(mini);
+}
+
+void	print_commands(t_mini *mini)
+{
+	t_commands	*current = mini->commands;
+	int			cmd_number = 1;
+	int			j = 0;
+
+	while (current != NULL)
+	{
+		printf("Command %d:\n", cmd_number++);
+		if (current->cmd_args != NULL)
+		{
+			for	(int i = 0; current->cmd_args[i] != NULL; i++)
+			{
+				printf("  arg[%d]: %s\n", i, current->cmd_args[i]);
+			}
+		}
+		if (current->input != NULL)
+		{
+			while (current->input[j])
+			{
+				printf("input = %s\n", current->input[j]);
+				printf("input indicator = %d\n", current->indicator_input[j]);
+				printf("order_idnex == %d\n", current->order_input[j]);
+				j++;
+			}
+		}
+		j = 0;
+		if (current->output != NULL)
+		{
+			while (current->output[j])
+			{
+				printf("output = %s\n", current->output[j]);
+				printf("output indicator = %d\n", current->indicator_output[j]);
+				printf("order_idnex == %d\n", current->order_output[j]);
+				j++;
+			}
+		}
+		//printf("exit code = %d\n", mini->exitcode);
+		current = current->next;
+	}
+	printf("-----------------------------\n");
 }
